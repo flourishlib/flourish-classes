391b9ebddc67becd78f8b8321ed82b18d8e2bc68
wbond <will@wbond.net>
2008-08-14 04:56:43 +0000

    Added fValidation::addEmailFields(), fValidation::addEmailHeaderFields() and fValidation::addRequiredFields()

--- classes/request/fValidation.php
+++ classes/request/fValidation.php
@@ -37,6 +37,63 @@ class fValidation
 	
 	
 	/**
+	 * Adds form fields to be required to be blank or a valid email address
+	 * 
+	 * Use {@link fValidation::setRequiredFields()} to not allow blank values.
+	 * 
+	 * @param  string $field,...  Any number of fields to required valid email addresses for
+	 * @return void
+	 */
+	public function addEmailFields()
+	{
+		$args = func_get_args();
+		$this->setEmailFields(array_merge($this->email_fields, $args));
+	}
+	
+	
+	/**
+	 * Adds form fields to be checked for email injection
+	 * 
+	 * Every field that is included in email headers should be passed to this
+	 * method.
+	 * 
+	 * @param  string $field,...  Any number of fields to be checked for email injection
+	 * @return void
+	 */
+	public function addEmailHeaderFields()
+	{
+		$args = func_get_args();
+		$this->setEmailHeaderFields(array_merge($this->email_header_fields, $args));
+	}
+	
+	
+	/**
+	 * Adds form fields to be required, taking each parameter as a field name
+	 * 
+	 * To require one of multiple fields, pass an array of fields as the parameter.
+	 * 
+	 * To conditionally require fields, pass an associative array of with the
+	 * key being the field that will trigger the other fields to be required:
+	 * <pre>
+	 * array(
+	 *     'trigger_field' => array(
+	 *         'conditionally_required_field',
+	 *         'second_conditionally_required_field'
+	 *     )
+	 * )
+	 * </pre>
+	 * 
+	 * @param  mixed $field,...  Any number of fields to check
+	 * @return void
+	 */
+	public function addRequiredFields()
+	{
+		$args = func_get_args();
+		$this->setRequiredFields(array_merge($this->required_fields, $args));	
+	}
+	
+	
+	/**
 	 * Validates the email fields, requiring that any email fields that have a value are formatted like an email address
 	 * 
 	 * @param  array &$messages  The messages to display to the user
