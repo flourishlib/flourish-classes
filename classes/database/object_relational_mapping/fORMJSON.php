2546bd3c1df485e0a81021e3e3fe0c726756b7a3
wbond <will@wbond.net>
2008-09-11 11:49:37 +0000

    Added fORMJSON, which provides the toJSON() method to fActiveRecord and fRecordSet classes

---
+++ classes/database/object_relational_mapping/fORMJSON.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Adds JSON functionality to fActiveRecord and fRecordSet
+ * 
+ * @copyright  Copyright (c) 2008 William Bond
+ * @author     William Bond [wb] <will@flourishlib.com>
+ * @license    http://flourishlib.com/license
+ * 
+ * @package    Flourish
+ * @link       http://flourishlib.com/fORMJSON
+ * 
+ * @version    1.0.0b
+ * @changes    1.0.0b  The initial implementation [wb, 2008-06-25]
+ */
+class fORMJSON
+{
+	/**
+	 * Adds the method toJSON() to fActiveRecord and fRecordSet instances
+	 * 
+	 * @return void
+	 */
+	static public function extend()
+	{
+		fORM::registerReflectCallback(
+			'*',
+			array('fORMJSON', 'reflect')
+		);
+		
+		fORM::registerHookCallback(
+			'*',
+			'replace::toJSON()',
+			array('fORMJSON', 'toJSON')
+		);
+		
+		fORM::registerHookCallback(
+			'*',
+			'replace::toArray()',
+			array('fORMJSON', 'toArray')
+		);
+		
+		fRecordSet::registerCallback(
+			'toJSON',
+			array('fORMJSON', 'toJSONRecordSet')
+		);
+	}
+	
+	
+	/**
+	 * Adjusts the {@link fActiveRecord::reflect()} signatures of columns that have been added by this class
+	 * 
+	 * @internal
+	 * 
+	 * @param  string  $class                 The class to reflect
+	 * @param  array   &$signatures           The associative array of {method name} => {signature}
+	 * @param  boolean $include_doc_comments  If doc comments should be included with the signature
+	 * @return void
+	 */
+	static public function reflect($class, &$signatures, $include_doc_comments)
+	{
+		$signature = '';
+		if ($include_doc_comments) {
+			$signature .= "/**\n";
+			$signature .= " * Converts the values from the record into a JSON object\n";
+			$signature .= " * \n";
+			$signature .= " * @return string  The JSON object representation of this record\n";
+			$signature .= " */\n";
+		}
+		$signature .= 'public function toJSON()';
+		
+		$signatures['toJSON'] = $signature;
+	}
+	
+	
+	/**
+	 * Returns the values array - used by {@link toJSONRecordSet()}
+	 * 
+	 * @internal
+	 * 
+	 * @param  fActiveRecord $object            The fActiveRecord instance
+	 * @param  array         &$values           The current values
+	 * @param  array         &$old_values       The old values
+	 * @param  array         &$related_records  Any records related to this record
+	 * @param  string        &$method_name      The method that was called
+	 * @param  array         &$parameters       The parameters passed to the method
+	 * @return string  The JSON object that represents the values of this record
+	 */
+	static public function toArray($object, &$values, &$old_values, &$related_records, &$method_name, &$parameters)
+	{
+		$output = array();
+		foreach ($values as $column => $value) {
+			if (is_object($value) && is_callable(array($value, '__toString'))) {
+				$value = $value->__toString();
+			}
+			$output[$column] = $value;
+		}		
+		
+		return $output;
+	}
+	
+	
+	/**
+	 * Returns a JSON object representation of the record
+	 * 
+	 * @internal
+	 * 
+	 * @param  fActiveRecord $object            The fActiveRecord instance
+	 * @param  array         &$values           The current values
+	 * @param  array         &$old_values       The old values
+	 * @param  array         &$related_records  Any records related to this record
+	 * @param  string        &$method_name      The method that was called
+	 * @param  array         &$parameters       The parameters passed to the method
+	 * @return string  The JSON object that represents the values of this record
+	 */
+	static public function toJSON($object, &$values, &$old_values, &$related_records, &$method_name, &$parameters)
+	{
+		$method = 'toArray';
+		$params = array();
+		return fJSON::encode(self::toArray($object, $values, $old_values, $related_records, $method, $params));
+	}
+	
+	
+	/**
+	 * Returns a JSON object representation of a record set
+	 * 
+	 * @internal
+	 * 
+	 * @param  fRecordSet $record_set  The fRecordSet instance
+	 * @param  string     $class       The class of the records
+	 * @param  array      &$records    The fActiveRecord objects
+	 * @param  integer    &$pointer    The current iteration pointer
+	 * @param  boolean    &$associate  If the record set should be associated with any containing fActiveRecord
+	 * @return string  The JSON object that represents an array of all of the fActiveRecord objects
+	 */
+	static public function toJSONRecordSet($record_set, $class, &$records, &$pointer, &$associate)
+	{
+		return fJSON::encode($record_set->call('toArray'));	
+	}
+	
+	
+	/**
+	 * Forces use as a static class
+	 * 
+	 * @return fORMJSON
+	 */
+	private function __construct() { }
+}
+
+
+
+/**
+ * Copyright (c) 2008 William Bond <will@flourishlib.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
