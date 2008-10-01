29b7d7b5b30d72e7d86dbbb84646827c62253fd8
wbond <will@wbond.net>
2008-10-01 04:19:08 +0000

    Removed the class fSQLParsing and merged the relevant bits into fSQLTranslation

--- classes/fSQLTranslation.php
+++ classes/fSQLTranslation.php
@@ -17,6 +17,129 @@
 class fSQLTranslation
 {
 	/**
+	 * Takes a Flourish SQL SELECT query and parses it into clauses.
+	 * 
+	 * The select statement must be of the format:
+	 * 
+	 * SELECT [ table_name. | alias. ]*
+	 * FROM table [ AS alias ] [ [ INNER | OUTER ] [ LEFT | RIGHT ] JOIN other_table ON condition | , ] ...
+	 * [ WHERE condition [ , condition ]... ]
+	 * [ GROUP BY conditions ]
+	 * [ HAVING conditions ]
+	 * [ ORDER BY [ column | expression ] [ ASC | DESC ] [ , [ column | expression ] [ ASC | DESC ] ] ... ]
+	 * [ LIMIT integer [ OFFSET integer ] ]
+	 * 
+	 * The returned array will contain the following keys, which may have a NULL or non-empty string value:
+	 *  - 'SELECT'
+	 *  - 'FROM'
+	 *  - 'WHERE'
+	 *  - 'GROUP BY'
+	 *  - 'HAVING'
+	 *  - 'ORDER BY'
+	 *  - 'LIMIT'
+	 * 
+	 * @param  string $sql  The SQL to parse
+	 * @return array  The various clauses of the SELECT statement (see method descript for details)
+	 */
+	static private function parseSelectSQL($sql)
+	{
+		// Split the strings out of the sql so parsing doesn't get messed up by quoted values
+		preg_match_all("#(?:'(?:''|\\\\'|\\\\[^']|[^'\\\\]+)*')|(?:[^']+)#", $sql, $matches);
+		
+		$possible_clauses = array('SELECT', 'FROM', 'WHERE', 'GROUP BY', 'HAVING', 'ORDER BY', 'LIMIT');
+		$found_clauses    = array();
+		foreach ($possible_clauses as $possible_clause) {
+			$found_clauses[$possible_clause] = NULL;
+		}
+		
+		$current_clause = 0;
+		
+		foreach ($matches[0] as $match) {
+			// This is a quoted string value, don't do anything to it
+			if ($match[0] == "'") {
+				$found_clauses[$possible_clauses[$current_clause]] .= $match;
+			
+			// Non-quoted strings should be checked for clause markers
+			} else {
+				
+				// Look to see if a new clause starts in this string
+				$i = 1;
+				while ($current_clause+$i < sizeof($possible_clauses)) {
+					// If the next clause is found in this string
+					if (stripos($match, $possible_clauses[$current_clause+$i]) !== FALSE) {
+						list($before, $after) = preg_split('#\s*' . $possible_clauses[$current_clause+$i] . '\s*#i', $match);
+						$found_clauses[$possible_clauses[$current_clause]] .= preg_replace('#\s*' . $possible_clauses[$current_clause] . '\s*#i', '', $before);
+						$match = $after;
+						$current_clause = $current_clause + $i;
+						$i = 0;
+					}
+					$i++;
+				}
+				
+				// Otherwise just add on to the current clause
+				if (!empty($match)) {
+					$found_clauses[$possible_clauses[$current_clause]] .= preg_replace('#\s*' . $possible_clauses[$current_clause] . '\s*#i', '', $match);
+				}
+			}
+		}
+		
+		return $found_clauses;
+	}
+	
+		
+	/**
+	 * Takes the FROM clause from {@link parseSelectSQL()} and returns all of the tables and each one's alias
+	 * 
+	 * @param  string $clause  The SQL clause to parse
+	 * @return array  The tables in the from clause, with the table alias being the key and value being the name
+	 */
+	static private function parseTableAliases($sql)
+	{
+		$aliases = array();
+		
+		preg_match_all("#(?:'(?:''|\\\\'|\\\\[^']|[^'\\\\]+)*')|(?:[^']+)#", $sql, $matches);
+		
+		$sql = '';
+		// Replace strings with two single quotes
+		foreach ($matches[0] as $match) {
+			if ($match[0] == "'") {
+				$match = "''";
+			}
+			$sql .= $match;
+		}
+		
+		// Turn comma joins into cross joins
+		if (preg_match('#^(?:\w+(?:\s+(?:as\s+)?(?:\w+))?)(?:\s*,\s*(?:\w+(?:\s+(?:as\s+)?(?:\w+))?))*$#is', $sql)) {
+			$sql = str_replace(',', ' CROSS JOIN ', $sql);
+		}
+		
+		// Error out if we can't figure out the join structure
+		if (!preg_match('#^(?:\w+(?:\s+(?:as\s+)?(?:\w+))?)(?:\s+(?:(?:CROSS|INNER|OUTER|LEFT|RIGHT)?\s+)*JOIN\s+(?:\w+(?:\s+(?:as\s+)?(?:\w+))?)(?:\s+ON\s+.*)?)*$#is', $sql)) {
+			fCore::toss(
+				'fProgrammerException',
+				fGrammar::compose(
+					'Unable to parse FROM clause, does not appears to be in comma style or join style'
+				)
+			);
+		}
+		
+		$tables = preg_split('#\s+((?:(?:CROSS|INNER|OUTER|LEFT|RIGHT)?\s+)*?JOIN)\s+#i', $sql);
+		
+		foreach ($tables as $table) {
+			// This grabs the table name and alias (if there is one)
+			preg_match('#\s*([\w.]+)(?:\s+(?:as\s+)?((?!ON)[\w.]+))?\s*(?:ON\s+(.*))?#im', $table, $parts);
+			
+			$table_name  = $parts[1];
+			$table_alias = (isset($parts[2])) ? $parts[2] : $parts[1];
+			
+			$aliases[$table_alias] = $table_name;
+		}
+		
+		return $aliases;
+	}
+	
+	
+	/**
 	 * Callback for custom SQLite function; calculates the cotangent of a number
 	 * 
 	 * @internal
@@ -348,8 +471,8 @@ class fSQLTranslation
 		$additions = array();
 		
 		foreach ($selects as $select) {
-			$clauses       = fSQLParsing::parseSelectSQL($select);
-			$table_aliases = fSQLParsing::parseTableAliases($clauses['FROM']);
+			$clauses       = self::parseSelectSQL($select);
+			$table_aliases = self::parseTableAliases($clauses['FROM']);
 			
 			preg_match_all('#([^,()\']+|\'(?>\'\'|\\\\\'|\\\\[^\']|[^\'\\\\]+)*\'|\((?:(?1)|,)*\)|\(\))+#i', $clauses['SELECT'], $selections);
 			$selections    = array_map('trim', $selections[0]);
@@ -812,7 +935,7 @@ class fSQLTranslation
 		preg_match_all('#((select(?:\s*(?:[^()\']+|\'(?:\'\'|\\\\\'|\\\\[^\']|[^\'\\\\]+)*\'|\((?1)\)|\(\))+\s*))\s+limit\s+(\d+)\s+offset\s+(\d+))#i', $sql, $matches, PREG_SET_ORDER);
 		
 		foreach ($matches as $match) {
-			$clauses = fSQLParsing::parseSelectSQL($match[1]);
+			$clauses = self::parseSelectSQL($match[1]);
 			
 			if ($clauses['ORDER BY'] == NULL) {
 				$clauses['ORDER BY'] = '1 ASC';
