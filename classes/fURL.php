866c04b1613c04d379399acc3a528428e22b0ff6
wbond <will@wbond.net>
2008-10-30 01:13:37 +0000

    Changed fURL::makeFriendly() to not allow . or ' in the output

--- classes/fURL.php
+++ classes/fURL.php
@@ -85,11 +85,9 @@ class fURL
 	static public function makeFriendly($string)
 	{
 		$string = fHTML::decode(fUTF8::ascii($string));
-		$string = strtolower($string);
-		$string = preg_replace("#[^a-zA-Z0-9\\. '-]#", ' ', $string);
-		$string = str_replace("'", '', $string);
-		$string = preg_replace('#\s+#', '_', trim($string));
-		return preg_replace('#\.$#', '', $string);
+		$string = strtolower(trim($string));
+		$string = preg_replace('#[^a-z0-9\-]+#', '_', $string);
+		return preg_replace('#_+$#', '', $string);
 	}
 	
 	
