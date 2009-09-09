a9befb9c24a2137aec14b295b9a3722987e18676
wbond <will@wbond.net>
2009-09-09 06:39:27 +0000

    Fixed ticket #297 - parsing table aliases in fSQLTranslation while handling MSSQL national columns was producing incorrect results

--- classes/fSQLTranslation.php
+++ classes/fSQLTranslation.php
@@ -9,7 +9,8 @@
  * @package    Flourish
  * @link       http://flourishlib.com/fSQLTranslation
  * 
- * @version    1.0.0b7
+ * @version    1.0.0b8
+ * @changes    1.0.0b8  Fixed a bug with parsing table aliases that occurs when handling MSSQL national columns [wb, 2009-09-09] 
  * @changes    1.0.0b7  Fixed a bug with translating `NOT LIKE` operators in PostgreSQL [wb, 2009-07-15]
  * @changes    1.0.0b6  Changed replacement values in preg_replace() calls to be properly escaped [wb, 2009-06-11]
  * @changes    1.0.0b5  Update code to only translate data types inside of `CREATE TABLE` queries [wb, 2009-05-22]
@@ -71,7 +72,7 @@ class fSQLTranslation
 			preg_match('#^\s*([\w.]+|\(((?:[^()]+|\((?2)\))*)\))(?:\s+(?:as\s+)?((?!ON|USING)[\w.]+))?\s*(?:(?:ON|USING)\s+(.*))?\s*$#im', $table, $parts);
 			
 			$table_name  = $parts[1];
-			$table_alias = (isset($parts[2])) ? $parts[2] : $parts[1]; 
+			$table_alias = (!empty($parts[2])) ? $parts[2] : $parts[1]; 
 			
 			$aliases[$table_alias] = $table_name;
 		}
